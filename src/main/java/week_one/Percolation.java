ed8509c90d581ac0e44a366206de42a0f09336c6
Georgy Plotnikov <plotnikov.georgy@x5.ru>
2019-02-15 16:56:45 +0400

    Initial commit.

---
+++ src/main/java/week_one/Percolation.java
@@ -0,0 +1,28 @@
+package week_one;
+
+public class Percolation {
+  public Percolation(int n) {}
+
+  public void open(int row, int col){}
+
+  public boolean isOpen(int row, int col) {
+    return false;
+  }
+
+  public boolean isFull(int row, int col) {
+    return false;
+  }
+
+  public int numberOfOpenSites() {
+    return 0;
+  }
+
+  public boolean percolates() {
+    return false;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
