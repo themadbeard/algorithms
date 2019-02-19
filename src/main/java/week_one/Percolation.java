b321f00f8f7dd26af7473f05fe66625a49f038e3
Georgy Plotnikov <plotnikov.georgy@x5.ru>
2019-02-19 17:20:24 +0400

    Week 1. Percolation task.

--- src/main/java/week_one/Percolation.java
+++ src/main/java/week_one/Percolation.java
@@ -1,28 +1,89 @@
 package week_one;
 
-public class Percolation {
-  public Percolation(int n) {}
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-  public void open(int row, int col){}
+public class Percolation {
+
+  private boolean[][] sites;
+  private final int size;
+  private final int top = 0;
+  private int opened = 0;
+  private int bottom;
+  private final WeightedQuickUnionUF quickUnionUF;
+
+  public Percolation(int n) {
+    if(n <= 0) {
+      throw new IllegalArgumentException();
+    }
+    size = n;
+    sites = new boolean[n][n];
+    bottom = (n * n) + 1;
+    quickUnionUF = new WeightedQuickUnionUF((n * n) + 2);
+  }
+
+  public void open(int row, int col) {
+    int currentSiteQFIndex = getQFIndex(row, col);
+
+    if(isOpen(row, col)) {
+      //already opened
+      return;
+    }
+
+    sites[row - 1][col - 1] = true;
+    opened++;
+
+    //top sites always connected with virtual top
+    if(row == 1) {
+      quickUnionUF.union(top, currentSiteQFIndex);
+    }
+
+    //bottom sites always connected with the virtual bottom
+    if(row == size) {
+      quickUnionUF.union(bottom, currentSiteQFIndex);
+    }
+
+    //union with left opened site
+    if(row > 1 && isOpen(row - 1, col)) {
+      quickUnionUF.union(currentSiteQFIndex, getQFIndex(row - 1, col));
+    }
+    //union with right opened site
+    if(row < size && isOpen(row + 1, col)) {
+      quickUnionUF.union(currentSiteQFIndex, getQFIndex(row + 1, col));
+    }
+
+    //union with top opened site
+    if(col > 1 && isOpen(row, col - 1)) {
+      quickUnionUF.union(currentSiteQFIndex, getQFIndex(row, col - 1));
+    }
+
+    //union with bottom opened site
+    if(col < size && isOpen(row, col + 1)) {
+      quickUnionUF.union(currentSiteQFIndex, getQFIndex(row, col + 1));
+    }
+  }
 
   public boolean isOpen(int row, int col) {
-    return false;
+    return sites[row - 1][col - 1];
   }
 
   public boolean isFull(int row, int col) {
-    return false;
+    if((row < 0 || row >= size) ||  (col < 0 || col >= size)) {
+      throw new IllegalArgumentException();
+    }
+    return quickUnionUF.connected(getQFIndex(row, col), top);
   }
 
   public int numberOfOpenSites() {
-    return 0;
+    return opened;
   }
 
   public boolean percolates() {
-    return false;
+    return quickUnionUF.connected(top, bottom);
   }
 
-  public static void main(String[] args) {
-
+  //Convert a two-dimensional array index into one-dimensional array index
+  // In UnionFind used an one-dimensional array.
+  private int getQFIndex(int row, int col) {
+    return (row - 1) * size + col;
   }
-
 }
--- src/main/java/week_one/PercolationStats.java
+++ src/main/java/week_one/PercolationStats.java
@@ -1,26 +1,52 @@
 package week_one;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
-  public PercolationStats(int n, int trials) {}
+  private final int numberOfExperiments;
+  private final double[] fractions;
+
+  private static final double CONDINDENCE_INTERVAL_PARAMETER = 1.96;
+
+  public PercolationStats(int n, int trials) {
+    if(n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
+    }
+
+    numberOfExperiments = trials;
+    fractions = new double[numberOfExperiments];
+
+    for(int i = 0; i < numberOfExperiments; ++i) {
+      Percolation percolation = new Percolation(n);
+
+      while(!percolation.percolates()) {
+        int row = StdRandom.uniform(1, n + 1);
+        int col = StdRandom.uniform(1, n + 1);
+        percolation.open(row, col);
+      }
+
+      double fraction = (double) percolation.numberOfOpenSites() / (n * n);
+      fractions[i] = fraction;
+    }
+
+  }
 
   public double mean() {
-    return 0.0;
+    return StdStats.mean(fractions);
   }
 
   public double stddev() {
-    return 0.0;
+    return StdStats.stddev(fractions);
   }
 
   public double confidenceLo() {
-    return 0.0;
+    return mean() - (CONDINDENCE_INTERVAL_PARAMETER * stddev()) / Math.sqrt(numberOfExperiments);
   }
 
   public double confidenceHi() {
-    return 0.0;
+    return mean() + (CONDINDENCE_INTERVAL_PARAMETER * stddev()) / Math.sqrt(numberOfExperiments);
   }
 
-  public static void main(String[] args) {
-
-  }
 }
