b32eee2294930fd78b30f232258096ed725eecf4
Georgy Plotnikov <plotnikov.georgy@x5.ru>
2019-05-06 14:13:23 +0400

    Week 4. 8 Puzzle. Final Implementation.

--- src/main/java/week_four/Board.java
+++ src/main/java/week_four/Board.java
@@ -1,5 +1,8 @@
 package week_four;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Board {
 
   private static final int SPACE = 0;
@@ -50,13 +53,54 @@ public class Board {
     return true;
   }
 
+  // a board that is obtained by exchanging any pair of blocks
   public Board twin() {
-    return null;
+    for (int row = 0; row < blocks.length; row++)
+      for (int col = 0; col < blocks.length - 1; col++)
+        if (!isSpaceBlock(block(row, col)) && !isSpaceBlock(block(row, col + 1)))
+          return new Board(swap(row, col, row, col + 1));
+    throw new RuntimeException();
   }
 
   public Iterable<Board> neighbors() {
-    return null;
+    Queue<Board> neighbors = new LinkedList<>();
+
+    int[] spaceLocation = spaceLocation();
+    int spaceRow = spaceLocation[0];
+    int spaceCol = spaceLocation[1];
+
+    // space moves UP
+    if (spaceRow - 1 >= 0) {
+      neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow - 1, spaceCol)));
+    }
+
+    // space moves DOWN
+    if (spaceRow + 1 < dimension) {
+      neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow + 1, spaceCol)));
+    }
+
+    // space moves LEFT
+    if (spaceCol - 1 >= 0) {
+      neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol - 1)));
+    }
+
+    // space moves RIGHT
+    if (spaceCol + 1 < dimension) {
+      neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol + 1)));
+    }
+
+    return neighbors;
   }
+
+  private int[][] swap(int row1, int col1, int row2, int col2) {
+    int[][] copy = copy(blocks);
+    int tmp = copy[row1][col1];
+    copy[row1][col1] = copy[row2][col2];
+    copy[row2][col2] = tmp;
+
+    return copy;
+  }
+
 
   @Override
   public boolean equals(Object y) {
@@ -68,7 +112,7 @@ public class Board {
       return true;
     }
 
-    if (y.getClass() != this.getClass()) {
+    if (!(y instanceof Board)) {
       return false;
     }
 
@@ -77,13 +121,32 @@ public class Board {
       return false;
     }
 
-    for (int i = 0; i < that.dimension() * that.dimension(); i++) {
-      if (this.blocks[i] != that.blocks[i]) {
-        return false;
+    for (int i = 0; i < that.dimension(); i++) {
+      for (int j = 0; j < that.dimension; j++) {
+        if (this.block(i, j) != that.block(i, j)) {
+          return false;
+        }
       }
     }
 
     return true;
+  }
+
+  // find space location
+  private int[] spaceLocation() {
+    int[] location = new int[2];
+
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        if(isSpaceBlock(block(i, j))) {
+          location[0] = i;
+          location[1] = j;
+          return location;
+        }
+      }
+    }
+
+    throw new RuntimeException("Space wan't found.");
   }
 
   @Override
--- src/main/java/week_four/Solver.java
+++ src/main/java/week_four/Solver.java
@@ -1,23 +1,93 @@
 package week_four;
 
+import edu.princeton.cs.algs4.MinPQ;
+import java.util.Stack;
+
 public class Solver {
 
-  private Board board;
+  private Node lastNode;
 
+  // find a solution to the initial board (using the A* algorithm)
   public Solver(Board initial) {
+    if(initial == null) {
+      throw new IllegalArgumentException();
+    }
 
+    MinPQ<Node> nodes = new MinPQ<>();
+    nodes.insert(new Node(initial));
+
+    MinPQ<Node> twinNodes = new MinPQ<>();
+    twinNodes.insert(new Node(initial.twin()));
+
+    while (true) {
+      lastNode = expand(nodes);
+      if (lastNode != null || expand(twinNodes) != null) {
+        return;
+      }
+    }
+  }
+
+  private Node expand(MinPQ<Node> Nodes) {
+    if (Nodes.isEmpty()) {
+      return null;
+    }
+
+    Node bestNode = Nodes.delMin();
+
+    if (bestNode.board.isGoal()) {
+      return bestNode;
+    }
+
+    for (Board neighbor : bestNode.board.neighbors()) {
+      // compare init state
+      if (bestNode.previous == null || !neighbor.equals(bestNode.previous.board)) {
+        Nodes.insert(new Node(neighbor, bestNode));
+      }
+    }
+    return null;
   }
 
   public boolean isSolvable() {
-    return false;
+    return lastNode != null;
   }
 
   public int moves() {
-    return 0;
+    return isSolvable() ? lastNode.numNodes : -1;
   }
 
   public Iterable<Board> solution() {
-    return null;
+    if (!isSolvable()) {
+      return null;
+    }
+
+    Stack<Board> Nodes = new Stack<>();
+    while (lastNode != null) {
+      Nodes.push(lastNode.board);
+      lastNode = lastNode.previous;
+    }
+
+    return Nodes;
+  }
+
+  private class Node implements Comparable<Node> {
+
+    private Node previous;
+    private Board board;
+    private int numNodes = 0;
+
+    public Node(Board board) {
+      this.board = board;
+    }
+
+    public Node(Board board, Node previous) {
+      this.board = board;
+      this.previous = previous;
+      this.numNodes = previous.numNodes + 1;
+    }
+
+    public int compareTo(Node Node) {
+      return (this.board.manhattan() - Node.board.manhattan()) + (this.numNodes - Node.numNodes);
+    }
   }
 
 }
